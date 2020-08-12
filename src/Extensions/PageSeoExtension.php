acd9d992216571ae9f96fd1b28a9cb3408bc07b1
Joe Madden <joe.madden@moosylvania.com>
2020-08-12 10:30:38 -0500

    Update PageSeoExtension.php
    change where fields are inserted

--- src/Extensions/PageSeoExtension.php
+++ src/Extensions/PageSeoExtension.php
@@ -7,8 +7,8 @@ use SilverStripe\Assets\Image;
 use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\ToggleCompositeField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\FieldType\DBDatetime;
@@ -87,19 +87,22 @@ class PageSeoExtension extends DataExtension
 
         $fields->addFieldsToTab('Root.Main', TextareaField::create('StructuredData'), 'ExtraMeta');
 
-        $fields->addFieldsToTab('Root.Main', [
-            ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
-                DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
-                TextField::create('FacebookPageTitle', 'Title')->setAttribute('placeholder', $this->getOwner()->Title)->setRightTitle('If blank, inherits default page title')->setTargetLength(45, 25, 70),
-                UploadField::create('FacebookPageImage', 'Image')->setRightTitle('Facebook recommends images to be 1200 x 630 pixels. If no image is provided, facebook will choose the first image that appears on the page which usually has bad results')->setFolderName('seo'),
-                TextareaField::create('FacebookPageDescription', 'Description')->setAttribute('placeholder', $this->getOwner()->MetaDescription ?: $this->getOwner()->dbObject('Content')->LimitCharacters(297))->setRightTitle('If blank, inherits meta description if it exists or gets the first 297 characters from content')->setTargetLength(200, 160, 320),
-            ]),
+        $fields->insertAfter('Metadata',
             ToggleCompositeField::create('TwitterSeoComposite', 'Twitter SEO', [
                 TextField::create('TwitterPageTitle', 'Title')->setAttribute('placeholder', $this->getOwner()->Title)->setRightTitle('If blank, inherits default page title')->setTargetLength(45, 25, 70),
                 UploadField::create('TwitterPageImage', 'Image')->setRightTitle('Must be at least 280x150 pixels')->setFolderName('seo'),
                 TextareaField::create('TwitterPageDescription', 'Description')->setAttribute('placeholder', $this->getOwner()->MetaDescription ?: $this->getOwner()->dbObject('Content')->LimitCharacters(297))->setRightTitle('If blank, inherits meta description if it exists or gets the first 297 characters from content')->setTargetLength(200, 160, 320),
             ])
-        ], 'Metadata');
+        );
+
+        $fields->insertAfter('Metadata',
+            ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
+                DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
+                TextField::create('FacebookPageTitle', 'Title')->setAttribute('placeholder', $this->getOwner()->Title)->setRightTitle('If blank, inherits default page title')->setTargetLength(45, 25, 70),
+                UploadField::create('FacebookPageImage', 'Image')->setRightTitle('Facebook recommends images to be 1200 x 630 pixels. If no image is provided, facebook will choose the first image that appears on the page which usually has bad results')->setFolderName('seo'),
+                TextareaField::create('FacebookPageDescription', 'Description')->setAttribute('placeholder', $this->getOwner()->MetaDescription ?: $this->getOwner()->dbObject('Content')->LimitCharacters(297))->setRightTitle('If blank, inherits meta description if it exists or gets the first 297 characters from content')->setTargetLength(200, 160, 320),
+            ])
+        );
     }
 
     /**
