b7caf1a1166244ef7045ca109ff4304be5fa45cb
Joe <joe.madden@moosylvania.com>
2025-06-13 11:47:28 -0500

    cleanup for adding in redirector and ignoring old dep

---
+++ src/Middleware/TrailingSlashRedirector.php
@@ -0,0 +1,118 @@
+<?php
+namespace QuinnInteractive\Seo\Middleware;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
+
+/**
+ * Ensure that a single trailing slash is always added to the URL.
+ * URLs accessed via Ajax, contain $_GET vars, or that contain
+ * an extension are ignored.
+ */
+class TrailingSlashRedirector implements HTTPMiddleware
+{
+    /**
+     * URLS to ignore
+     *
+     * @var array
+     */
+    private static $ignore_paths = [
+        'admin/',
+        'dev/',
+    ];
+
+    /**
+     * User-Agents to ignore
+     *
+     * @var array
+     */
+    private static $ignore_agents = [
+        'silverstripe/staticpublishqueue',
+    ];
+
+    /**
+     * Redirection status code
+     *
+     * @var int
+     */
+    private static $redirection_status_code = 301;
+
+    /**
+     * Process request
+     *
+     * @param HTTPRequest $request  HTTP request
+     * @param callable    $delegate Delegate
+     *
+     * @return HTTPResponse
+     */
+    public function process(HTTPRequest $request, callable $delegate)
+    {
+        $ignore_paths = [];
+
+        $ignore_config = Config::inst()
+            ->get(TrailingSlashRedirector::class, 'ignore_paths');
+
+        foreach ($ignore_config as $iurl) {
+            if ($quoted = preg_quote(ltrim($iurl, '/'), '/')) {
+                array_push($ignore_paths, $quoted);
+            }
+        }
+
+        if ($request && ($request->isGET() || $request->isHEAD())) {
+            // skip $ignore_paths and home (`/`)
+            if ($request->getURL() == ''
+                || preg_match(
+                    '/^(' . implode('|', $ignore_paths) . ')/i',
+                    $request->getURL()
+                )
+            ) {
+                return $delegate($request);
+            }
+
+            $requested_url = Environment::getEnv('REQUEST_URI');
+
+            $expected_url = rtrim(
+                Director::baseURL() . $request->getURL(), '/'
+            ) . '/';
+
+            $urlPathInfo = pathinfo($requested_url);
+
+            $params = $request->getVars();
+
+            $ignore_agents = Config::inst()
+                ->get(TrailingSlashRedirector::class, 'ignore_agents');
+
+            if (!Director::is_ajax()
+                && !($ignore_agents && in_array(
+                    $request->getHeader('User-Agent'),
+                    $ignore_agents
+                ))
+                && !isset($urlPathInfo['extension'])
+                && empty($params)
+                && !preg_match(
+                    '/^' . preg_quote($expected_url, '/') . '(?!\/)/i',
+                    $requested_url
+                )
+            ) {
+                $params       = $request->getVars();
+                $redirect_url = Controller::join_links($expected_url, '/');
+                $response     = new HTTPResponse();
+                $code         = Config::inst()->get(
+                    TrailingSlashRedirector::class,
+                    'redirection_status_code'
+                );
+
+                return $response->redirect($redirect_url, $code);
+            }
+        }
+
+        $response = $delegate($request);
+
+        return $response;
+    }
+}
